9a3e9a45d7fd5ab0baecdf78f2d1c1ea057e5bbf
Mohammad <162719285+moded12@users.noreply.github.com>
2025-12-05 11:05:35 +0300

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-  'db_host' => 'localhost',
-  'db_name' => 'appliance',
-  'db_user' => 'appliance',
-  'db_pass' => 'Tvvcrtv1610@',
-  'app_name' => 'متجر الأجهزة المنزلية',
-  'base_url' => 'https://www.shneler.com/xx'
-];
